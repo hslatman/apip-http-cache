842f16c962b7915568795dd572281d37ccf2598d
Herman Slatman <hermanslatman@hotmail.com>
2019-05-07 21:33:02 +0200

    Setup custom Purger by overriding api_platform.http_cache.purger
    We've created a custom Purger class that implements the API Platform
    PurgerInterface. The Purger purge() function is already called when a POST
    is done to an ApiResource. We need to fix the logic for triggering
    the purge in FosHttpCache.

--- src/Cache/Purger.php
+++ src/Cache/Purger.php
@@ -7,8 +7,54 @@
 
 namespace App\Cache;
 
+use ApiPlatform\Core\HttpCache\PurgerInterface;
 
-class Purger
+class Purger implements PurgerInterface
 {
+//    public function purge(array $iris)
+//    {
+//        // TODO: Implement purge() method.
+//    }
+
+//    private $clients;
+//
+//    /**
+//     * @param ClientInterface[] $clients
+//     */
+//    public function __construct(array $clients)
+//    {
+//        $this->clients = $clients;
+//    }
+//    public function __construct()
+//    {
+//        dd('here');
+//    }
+//
+
+    /**
+     * {@inheritdoc}
+     */
+    public function purge(array $iris)
+    {
+
+        if (!$iris) {
+            return;
+        }
+
+        // Create the regex to purge all tags in just one request
+        $parts = array_map(function ($iri) {
+            return sprintf('(^|\,)%s($|\,)', preg_quote($iri));
+        }, $iris);
+
+        $regex = \count($parts) > 1 ? sprintf('(%s)', implode(')|(', $parts)) : array_shift($parts);
+
+//        foreach ($this->clients as $client) {
+//            $client->request('BAN', '', ['headers' => ['ApiPlatform-Ban-Regex' => $regex]]);
+//        }
+
+        // TODO: purge the FosHttpCache
+        // Is the key correct, we're using?
+
+    }
 
 }
