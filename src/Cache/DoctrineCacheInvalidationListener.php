dfb5eb1c78a01d652dec78065d2379f144c83759
Herman Slatman <hermanslatman@hotmail.com>
2019-06-21 10:29:14 +0200

    Refactor cache invalidation for collection routes through routing mechanism

--- src/Cache/DoctrineCacheInvalidationListener.php
+++ src/Cache/DoctrineCacheInvalidationListener.php
@@ -3,6 +3,10 @@
  * Author: Herman Slatman
  * Date: 2019-05-24
  * Time: 16:22
+ *
+ * NOTE: this file was heavily inspired on the PurgeHttpCacheListener in API Platform:
+ * https://github.com/api-platform/core/blob/44a686cd9c047520d809e0c529b368822e9bb948/src/Bridge/Doctrine/EventListener/PurgeHttpCacheListener.php
+ *
  */
 
 namespace App\Cache;
@@ -12,10 +16,15 @@ use ApiPlatform\Core\Api\OperationType;
 use ApiPlatform\Core\Bridge\Symfony\Routing\RouteNameResolver;
 use ApiPlatform\Core\Bridge\Symfony\Routing\RouteNameResolverInterface;
 use ApiPlatform\Core\Exception\InvalidArgumentException;
+use ApiPlatform\Core\Exception\RuntimeException;
 use App\Entity\Fix;
+use Doctrine\Common\Util\ClassUtils;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\PersistentCollection;
 use FOS\HttpCacheBundle\CacheManager;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class DoctrineCacheInvalidationListener
 {
@@ -32,10 +41,29 @@ class DoctrineCacheInvalidationListener
     /** @var RouteNameResolverInterface $resolver */
     private $resolver;
 
+    /** @var \Symfony\Component\PropertyAccess\PropertyAccessor $property_accessor */
+    private $property_accessor;
+
     public function __construct(CacheManager $manager, RouteNameResolver $resolver)
     {
         $this->manager = $manager;
         $this->resolver = $resolver;
+        $this->property_accessor = PropertyAccess::createPropertyAccessor();
+    }
+
+    public function preUpdate(PreUpdateEventArgs $args): void
+    {
+        $object = $args->getObject();
+        $this->gatherResourceRoutes($object);
+        $change_set = $args->getEntityChangeSet();
+        $association_mappings = $args->getEntityManager()->getClassMetadata(ClassUtils::getClass($object))->getAssociationMappings();
+        foreach ($change_set as $key => $value) {
+            if (!isset($association_mappings[$key])) {
+                continue;
+            }
+            $this->addRoutesFor($value[0]);
+            $this->addRoutesFor($value[1]);
+        }
     }
 
     public function onFlush(OnFlushEventArgs $args) {
@@ -51,64 +79,108 @@ class DoctrineCacheInvalidationListener
         // something is handled within a service. Of course we could make this configurable in some way, though...
 
         // We also should always update collections/items when there are related entities
-        // TODO: we need to look into pagination of the result sets
 
         foreach ($uow->getScheduledEntityInsertions() as $entity) {
-            $this->gatherResourceAndItemClasses($entity, false);
-            $this->gatherRelationClasses($em, $entity);
+            // NOTE: including the entity insertions, because we might be running in a service, not in a request
+            $this->gatherResourceRoutes($entity);
+            $this->gatherRelationRoutes($em, $entity);
         }
         foreach ($uow->getScheduledEntityUpdates() as $entity) {
-            $this->gatherResourceAndItemClasses($entity, true);
-            $this->gatherRelationClasses($em, $entity);
+            $this->gatherResourceRoutes($entity);
+            $this->gatherRelationRoutes($em, $entity);
         }
         foreach ($uow->getScheduledEntityDeletions() as $entity) {
-            $this->gatherResourceAndItemClasses($entity, true);
-            $this->gatherRelationClasses($em, $entity);
-        }
-
-
-        foreach($this->collection_routes as $route) {
-            $this->manager->invalidateRoute($route);
-        }
-
-        foreach($this->item_routes as $item) {
-            $route = $item['route'];
-            $parameters = $item['parameters'];
-            $this->manager->invalidateRoute($route, $parameters);
+            $this->gatherResourceRoutes($entity);
+            $this->gatherRelationRoutes($em, $entity);
         }
 
     }
 
-    private function gatherResourceAndItemClasses($entity, bool $purgeItem): void
+    public function postFlush(): void
+    {
+        if (empty($this->collection_routes)) {
+            return;
+        }
+
+        $unique_collection_routes = array_unique($this->collection_routes);
+
+        foreach($unique_collection_routes as $route) {
+            $this->manager->invalidateRoute($route);
+        }
+
+        $this->collection_routes = [];
+
+    }
+
+    private function gatherResourceRoutes($entity): void
     {
 
         // TODO: filter doubles? i.e. the ones that are triggerd automatically by symfony/cache already?
 
         try {
-            if (\get_class($entity) !== Fix::class) {
+            $class = ClassUtils::getClass($entity);
+            if ($class !== Fix::class) {
                 return;
             }
-            $route = $this->resolver->getRouteName(Fix::class, OperationType::COLLECTION);
+
+            $route = $this->resolver->getRouteName($class, OperationType::COLLECTION);
             $this->collection_routes[] = $route;
-            if ($purgeItem) {
-                $route = $this->resolver->getRouteName(Fix::class, OperationType::ITEM);
-                $parameters = ['id' => $entity->getId()]; // TODO: assuming an id is set; can we do better?
-                $this->item_routes[] = ['route' => $route, 'parameters' => $parameters];
-            }
+
         } catch (InvalidArgumentException $e) {
             return;
         }
     }
 
-    private function gatherRelationClasses(EntityManagerInterface $em, $entity): void
+    private function gatherRelationRoutes(EntityManagerInterface $em, $entity): void
     {
-        if (\get_class($entity) !== Fix::class) {
+        $class = ClassUtils::getClass($entity);
+        if ($class !== Fix::class) {
             return;
         }
-//        $associationMappings = $em->getClassMetadata(ClassUtils::getClass($entity))->getAssociationMappings();
-//        foreach (array_keys($associationMappings) as $property) {
-//            $this->addTagsFor($this->propertyAccessor->getValue($entity, $property));
-//        }
+
+        $association_mappings = $em->getClassMetadata($class)->getAssociationMappings();
+        foreach (array_keys($association_mappings) as $property) {
+            $this->addRoutesFor($this->property_accessor->getValue($entity, $property));
+        }
     }
 
+    private function addRoutesFor($value) : void
+    {
+        if (!$value) {
+            return;
+        }
+
+        if (!is_iterable($value)) {
+            $this->addTagForItem($value);
+            return;
+        }
+
+        if ($value instanceof PersistentCollection) {
+            $value = clone $value;
+        }
+
+        foreach ($value as $v) {
+            $this->addTagForItem($v);
+        }
+
+    }
+
+    private function addTagForItem($value): void
+    {
+        try {
+            $class = ClassUtils::getClass($value);
+            if ($class !== Fix::class) {
+                return;
+            }
+
+            $route = $this->resolver->getRouteName($class, OperationType::COLLECTION);
+            $this->collection_routes[] = $route;
+
+        } catch (InvalidArgumentException $e) {
+
+        } catch (RuntimeException $e) {
+
+        }
+
+    }
 }
